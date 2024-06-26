edc2a69908574eb2f4083495d39245de74071d9f
thesmitpatel <61879629+thesmitpatel@users.noreply.github.com>
2024-06-25 23:35:42 -0400

    Project set up and Database connection

---
+++ Ecommerce/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ecommerce.Models
+{
+    public class Category
+    {
+        [Key]
+        public int id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+    }
+}
