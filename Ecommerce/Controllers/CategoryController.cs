04956cb05e19970f6b79787e2662e409c303c838
thesmitpatel <61879629+thesmitpatel@users.noreply.github.com>
2024-06-30 13:29:27 -0400

    Get Category data from Database

---
+++ Ecommerce/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using Ecommerce.Models;
+using EcommerceWeb.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EcommerceWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db) 
+        {
+            _db = db;
+        
+        }
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+    }
+}
--- Ecommerce/Data/ApplicationDbContext.cs
+++ Ecommerce/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Ecommerce.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace EcommerceWeb.Data
 {
@@ -7,6 +8,17 @@ namespace EcommerceWeb.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options) : base(options)
         {
             
+        }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { id = 1, Name = "Action", DisplayOrder = 1 },
+                new Category { id = 2, Name = "SciFi", DisplayOrder = 2 },
+                new Category { id = 3, Name = "History", DisplayOrder = 3 }
+
+                );
         }
     }
 }
