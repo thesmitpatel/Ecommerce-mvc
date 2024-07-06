e2132ec07ec3c4a11d5812994014b6c9cdf0aaac
thesmitpatel <61879629+thesmitpatel@users.noreply.github.com>
2024-07-06 01:04:55 -0400

    chore: Create repository and CategoryRepository
    - Established a new repository for the project
    - Created CategoryRepository for better data management and separation of concerns

--- Ecommerce/Controllers/CategoryController.cs
+++ Ecommerce/Controllers/CategoryController.cs
@@ -1,20 +1,21 @@
 ﻿using Ecommerce.Models;
 using Ecommerce.DataAccess.Data;
 using Microsoft.AspNetCore.Mvc;
+using Ecommerce.DataAccess.Repository.IRepository;
 
 namespace EcommerceWeb.Controllers
 {
     public class CategoryController : Controller
     {
-        private readonly ApplicationDbContext _db;
-        public CategoryController(ApplicationDbContext db) 
+        private readonly ICategoryRepository _categoryRepo;
+        public CategoryController(ICategoryRepository db) 
         {
-            _db = db;
+            _categoryRepo = db;
         
         }
         public IActionResult Index()
         {
-            List<Category> objCategoryList = _db.Categories.ToList();
+            List<Category> objCategoryList = _categoryRepo.GetAll().ToList();
             return View(objCategoryList);
         }
 
@@ -29,8 +30,8 @@ namespace EcommerceWeb.Controllers
                 ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
             }
             if (ModelState.IsValid) {
-                _db.Categories.Add(obj);
-                _db.SaveChanges();
+                _categoryRepo.Add(obj);
+                _categoryRepo.Save();
                 TempData["success"] = "Category Created Successfully";
                 return RedirectToAction("Index", "Category");
             }
@@ -42,7 +43,7 @@ namespace EcommerceWeb.Controllers
             if (id == null || id == 0) {
                 return NotFound();
             }
-            Category? categoryFromDb = _db.Categories.FirstOrDefault(u => u.id == id);
+            Category? categoryFromDb = _categoryRepo.Get(u => u.id == id);
             if (categoryFromDb == null) {
                 return NotFound();
             }
@@ -53,8 +54,8 @@ namespace EcommerceWeb.Controllers
         {
             if (ModelState.IsValid)
             {
-                _db.Categories.Update(obj);
-                _db.SaveChanges();
+                _categoryRepo.Update(obj);
+                _categoryRepo.Save();
                 TempData["success"] = "Category Edited Successfully";
                 return RedirectToAction("Index", "Category");
             }
@@ -66,7 +67,7 @@ namespace EcommerceWeb.Controllers
             {
                 return NotFound();
             }
-            Category? categoryFromDb = _db.Categories.FirstOrDefault(u => u.id == id);
+            Category? categoryFromDb = _categoryRepo.Get(u => u.id == id);
             if (categoryFromDb == null)
             {
                 return NotFound();
@@ -76,12 +77,12 @@ namespace EcommerceWeb.Controllers
         [HttpPost, ActionName("Delete")]
         public IActionResult DeletePOST(int? id)
         {
-            Category? obj = _db.Categories.Find(id);
+            Category? obj = _categoryRepo.Get(u => u.id == id);
             if (obj == null) {
                 return NotFound();
             }
-            _db.Categories.Remove(obj);
-            _db.SaveChanges();
+            _categoryRepo.Remove(obj);
+            _categoryRepo.Save();
             TempData["success"] = "Category Deleted Successfully";
             return RedirectToAction("Index", "Category");
         }
--- Ecommerce/Program.cs
+++ Ecommerce/Program.cs
@@ -1,4 +1,5 @@
 using Ecommerce.DataAccess.Data;
+using Ecommerce.DataAccess.Repository.IRepository;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,6 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options=> 
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 
 var app = builder.Build();
 
